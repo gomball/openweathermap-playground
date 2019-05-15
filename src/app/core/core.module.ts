f093c77409b46dd7e6896efc5c0f5ee0afc1fdc9
gomball <dgomez250@gmail.com>
2019-05-15 22:44:37 +0200

    new app service locator
    for easing repetitive injections

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { Injector, NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { AppServiceLocator } from './services/app-service.locator';
 
 @NgModule({
   imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, RouterModule],
@@ -12,10 +13,12 @@ export class CoreModule {
   constructor(
     @Optional()
     @SkipSelf()
-    parentModule: CoreModule
+    parentModule: CoreModule,
+    injector: Injector
   ) {
     if (!!parentModule) {
       console.error('"CoreModule" already loaded. Import it _ONLY_ on main "AppModule".');
     }
+    AppServiceLocator.injector = injector;
   }
 }
