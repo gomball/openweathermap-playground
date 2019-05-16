278ae5a78d53fe5d2d37c6910c0f2b7e0d10428c
gomball <dgomez250@gmail.com>
2019-05-17 00:46:00 +0200

    preload sample city db

--- src/app/features/city-selector/city-selector.module.ts
+++ src/app/features/city-selector/city-selector.module.ts
@@ -4,10 +4,15 @@ import { MatAutocompleteModule, MatChipsModule, MatFormFieldModule } from '@angu
 import { SharedModule } from '../../shared/shared.module';
 import { CitySelectorRoutingModule } from './city-selector-routing.module';
 import { CitySelectorComponent } from './components/city-selector/city-selector.component';
+import { CityService } from './state/city.service';
 
 @NgModule({
   imports: [ReactiveFormsModule, MatAutocompleteModule, MatFormFieldModule, MatChipsModule, SharedModule, CitySelectorRoutingModule],
   declarations: [CitySelectorComponent],
   exports: [CitySelectorComponent]
 })
-export class CitySelectorModule {}
+export class CitySelectorModule {
+  constructor(cityService: CityService) {
+    cityService.loadCityDb();
+  }
+}
