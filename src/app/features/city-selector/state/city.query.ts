c16549c8b862c44f90b928504f2d1a1aa268ac5c
gomball <dgomez250@gmail.com>
2019-05-17 00:46:31 +0200

    city state business logic

--- src/app/features/city-selector/state/city.query.ts
+++ src/app/features/city-selector/state/city.query.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
+import { City } from '../../../domain/city';
 import { CityState, CityStore } from './city.store';
 
 @Injectable({ providedIn: 'root' })
 export class CityQuery extends Query<CityState> {
   available$ = this.select((state) => state.available);
   selected$ = this.select((state) => state.selected);
+
+  get available(): City[] {
+    return this.getValue().available;
+  }
+
+  get selected(): City[] {
+    return this.getValue().selected;
+  }
 
   constructor(protected store: CityStore) {
     super(store);
--- src/app/features/city-selector/state/city.service.ts
+++ src/app/features/city-selector/state/city.service.ts
@@ -1,7 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AppServiceLocator } from '../../../core/services/app-service-locator';
+import { City } from '../../../domain/city';
 import { CityStore } from './city.store';
 
 @Injectable({ providedIn: 'root' })
 export class CityService {
-  constructor(private readonly _store: CityStore) {}
+  private readonly _httpClient: HttpClient;
+
+  constructor(private readonly _store: CityStore) {
+    this._httpClient = AppServiceLocator.injector.get(HttpClient);
+  }
+
+  loadCityDb(): void {
+    const url = 'assets/data/cities.json';
+    this._httpClient.get<City[]>(url).subscribe((available) => this._store.update((state) => ({ ...state, available })));
+  }
+
+  addSelectedCity(city: City): void {
+    this._store.update((state) => {
+      const selected = [...state.selected, city];
+      return { ...state, selected };
+    });
+  }
+
+  removeSelectedCity(city: City): void {
+    this._store.update((state) => {
+      const selected = state.selected.filter((c) => c.id !== city.id);
+      return { ...state, selected };
+    });
+  }
 }
