794defae52e337654a4205190222b0f26db8cdcb
gomball <dgomez250@gmail.com>
2019-05-17 00:47:51 +0200

    city selector working in absence of layout

--- src/app/features/city-selector/components/city-selector/city-selector.component.ts
+++ src/app/features/city-selector/components/city-selector/city-selector.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ENTER } from '@angular/cdk/keycodes';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { City } from '../../../../domain/city';
+import { CityQuery } from '../../state/city.query';
+import { CityService } from '../../state/city.service';
 
 @Component({
   selector: 'owm-pg-city-selector',
@@ -6,11 +14,35 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./city-selector.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CitySelectorComponent implements OnInit {
+export class CitySelectorComponent {
+  ENTER = ENTER;
+  citiesFormControl = new FormControl();
+  availableFilteredCities$: Observable<City[]>;
 
-  constructor() { }
+  @ViewChild('cityInput') cityInput: ElementRef<HTMLInputElement>;
 
-  ngOnInit() {
+  constructor(private readonly _cityService: CityService, public readonly cityQuery: CityQuery) {
+    this.availableFilteredCities$ = this.citiesFormControl.valueChanges.pipe(
+      startWith(null),
+      map((searchValue: string) =>
+        !!searchValue
+          ? this.cityQuery.available.filter((city) => city.name.toLowerCase().includes(searchValue))
+          : [...this.cityQuery.available]
+      ),
+      map((available) => {
+        const selectedIds = this.cityQuery.selected.map((s) => s.id);
+        return available.filter((a) => !selectedIds.includes(a.id));
+      })
+    );
   }
 
+  onRemoved(city: City): void {
+    this._cityService.removeSelectedCity(city);
+  }
+
+  onSelected(event: MatAutocompleteSelectedEvent): void {
+    this._cityService.addSelectedCity((event.option.value as unknown) as City);
+    this.cityInput.nativeElement.value = '';
+    this.citiesFormControl.setValue(null);
+  }
 }
