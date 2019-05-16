5ef1d65ddf8d422d2a1a94b5476da22a176948cc
gomball <dgomez250@gmail.com>
2019-05-16 20:18:19 +0200

    fix some ts version related issues

--- src/app/core/types/translation-loader.ts
+++ src/app/core/types/translation-loader.ts
@@ -1,13 +1,8 @@
-declare var System: System;
-interface System {
-  import(request: string): Promise<any>;
-}
-
 import { TranslateLoader as NgxTranslateLoader } from '@ngx-translate/core';
 import { Observable, from } from 'rxjs';
 
 export class TranslationLoader implements NgxTranslateLoader {
   getTranslation(lang: string): Observable<any> {
-    return from(System.import(`../assets/i18n/${lang}.json`));
+    return from(import(`../../../assets/i18n/${lang}.json`));
   }
 }
