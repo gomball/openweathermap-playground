e67f2ede454b52252453c0d20a5812c5d7131036
gomball <dgomez250@gmail.com>
2019-05-16 22:37:55 +0200

    new shared (for features) module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  exports: [CommonModule, MatButtonModule, MatIconModule]
+})
+export class SharedModule {}
