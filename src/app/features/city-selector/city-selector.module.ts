0ac15b35143523d5684f9da83b120fc0bda1cea1
gomball <dgomez250@gmail.com>
2019-05-17 00:45:41 +0200

    city selector: add needed dependencies

--- src/app/features/city-selector/city-selector.module.ts
+++ src/app/features/city-selector/city-selector.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule, MatChipsModule, MatFormFieldModule } from '@angular/material';
 import { SharedModule } from '../../shared/shared.module';
 import { CitySelectorRoutingModule } from './city-selector-routing.module';
 import { CitySelectorComponent } from './components/city-selector/city-selector.component';
 
 @NgModule({
-  imports: [SharedModule, CitySelectorRoutingModule],
+  imports: [ReactiveFormsModule, MatAutocompleteModule, MatFormFieldModule, MatChipsModule, SharedModule, CitySelectorRoutingModule],
   declarations: [CitySelectorComponent],
   exports: [CitySelectorComponent]
 })
