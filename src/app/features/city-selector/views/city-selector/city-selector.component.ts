423aa800be002165558ba4feb0073da6b77943c0
gomball <dgomez250@gmail.com>
2019-05-18 00:29:18 +0200

    add validation to city selector form control

--- src/app/features/city-selector/views/city-selector/city-selector.component.ts
+++ src/app/features/city-selector/views/city-selector/city-selector.component.ts
@@ -1,8 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { AbstractControl, FormControl, ValidatorFn } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { CityService } from '../../state/city.service';
+
+const lengthValidator: (length: number, mode: 'max' | 'min') => ValidatorFn = (length: number, mode: 'max' | 'min') => {
+  return (control: AbstractControl): { [key: string]: any } => {
+    const value = control.value as any[];
+    const valid = mode === 'max' ? value.length <= length : value.length >= length;
+    return valid ? null : { minLength: { value } };
+  };
+};
 
 @Component({
   selector: 'owm-pg-city-selector',
@@ -12,7 +20,7 @@ import { CityService } from '../../state/city.service';
 })
 export class CitySelectorComponent implements OnDestroy {
   private _destrotSubj = new Subject<void>();
-  citiesFormControl = new FormControl();
+  citiesFormControl = new FormControl([], [lengthValidator(1, 'min'), lengthValidator(4, 'max')]);
 
   constructor(cityService: CityService) {
     this.citiesFormControl.valueChanges
