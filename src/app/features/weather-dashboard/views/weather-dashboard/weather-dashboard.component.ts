be1631398cc40907c7ab973a8bf30a26cc7ce944
gomball <dgomez250@gmail.com>
2019-05-19 17:32:39 +0200

    weather dashboard component request countdown, layout

--- src/app/features/weather-dashboard/views/weather-dashboard/weather-dashboard.component.ts
+++ src/app/features/weather-dashboard/views/weather-dashboard/weather-dashboard.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CityQuery } from '../../../../features/city-selector/state/city.query';
+import { FormControl } from '@angular/forms';
+import { timer } from 'rxjs';
+import { map, startWith, switchMap } from 'rxjs/operators';
+import { CityQuery } from '../../../city-selector/state/city.query';
+
+const REQUEST_DELAY_INITIAL_VALUE = 180;
 
 @Component({
   selector: 'owm-pg-weather-dashboard',
@@ -8,5 +13,11 @@ import { CityQuery } from '../../../../features/city-selector/state/city.query';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeatherDashboardComponent {
-  constructor(public readonly cityQuery: CityQuery) { }
+  requestDelayFormControl = new FormControl(REQUEST_DELAY_INITIAL_VALUE);
+  owmRequestTimer$ = this.requestDelayFormControl.valueChanges.pipe(
+    startWith(this.requestDelayFormControl.value),
+    switchMap((requestDelay: number) => timer(0, 1000).pipe(map((t) => this.requestDelayFormControl.value - (t % requestDelay) - 1)))
+  );
+
+  constructor(public readonly cityQuery: CityQuery) {}
 }
