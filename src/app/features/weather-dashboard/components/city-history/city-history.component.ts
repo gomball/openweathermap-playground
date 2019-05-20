a51a81c1314d972d94bf81e6606041c638801834
gomball <dgomez250@gmail.com>
2019-05-20 10:49:05 +0200

    city history modal component working

--- src/app/features/weather-dashboard/components/city-history/city-history.component.ts
+++ src/app/features/weather-dashboard/components/city-history/city-history.component.ts
@@ -1,7 +1,10 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { ChangeDetectionStrategy, Component, Inject, OnDestroy } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { City } from '../../../../domain/city';
-import { CityHistoryRecord } from '../../state/city-history.store';
+import { CurrentWeatherContract } from '../../services/owm/owm-dto.contracts';
+import { CityHistoryQuery } from '../../state/city-history.query';
 
 export interface CityHistoryComponentInput {
   city: City;
@@ -13,9 +16,21 @@ export interface CityHistoryComponentInput {
   styleUrls: ['./city-history.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CityHistoryComponent {
+export class CityHistoryComponent implements OnDestroy {
+  private readonly _destroySubj = new Subject<void>();
+  gridColumns = ['dateTime', 'condition', 'temperature', 'pressure', 'humidity', 'cloudiness', 'wind'];
+  history$: Observable<CurrentWeatherContract[]>;
+
   constructor(
+    cityHistoryQuery: CityHistoryQuery,
     public readonly dialogRef: MatDialogRef<CityHistoryComponent>,
     @Inject(MAT_DIALOG_DATA) public readonly data: CityHistoryComponentInput
-  ) {}
+  ) {
+    this.history$ = cityHistoryQuery.getCityHistory$(data.city.id).pipe(takeUntil(this._destroySubj.asObservable()));
+  }
+
+  ngOnDestroy(): void {
+    this._destroySubj.next();
+    this._destroySubj.unsubscribe();
+  }
 }
